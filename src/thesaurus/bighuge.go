dd3e45696b6adeae625916f58860d59d24a58acb
Adit0507 <adi05072002@gmail.com>
2025-01-11 18:54:31 +0530

    thesaurus

---
+++ src/thesaurus/bighuge.go
@@ -0,0 +1,44 @@
+package thesaurus
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+)
+
+// defines neccesary API key
+// is provides the Synonym method thats responsible for doing work of accessing the endpoint
+type BigHuge struct {
+	APIKey string
+}
+
+type words struct {
+	Syn []string `json:"syn"`
+}
+
+type synonyms struct {
+	Noun *words `json:"noun"`
+	Verb *words `json:"verb"`
+}
+
+func (b *BigHuge) Synonyms(term string) ([]string, error) {
+	var syns []string
+	resp, err := http.Get("http://words.bighugelabs.com/api/2/" + b.APIKey + "/" + term + "/json")
+	if err != nil {
+		return syns, errors.New("bighuge: Failed when looking for synonyms for " + term + "" + err.Error())
+	}
+
+	var data synonyms
+	defer resp.Body.Close()
+	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
+		return syns, err
+	}
+	if data.Noun != nil {
+		syns = append(syns, data.Noun.Syn...)
+	}
+	if data.Verb != nil {
+		syns = append(syns, data.Verb.Syn...)
+	}
+
+	return syns, nil
+}
